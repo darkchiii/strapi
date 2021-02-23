66d75ae9b7ef052c6fba812cd661f7e49df0dedf
ELO NARA <elonara@MacBook-Air.local>
2021-02-23 16:36:40 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fc7ced36bbf37548306c92022c65b8ea'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a1e3df56-a582-41d8-bd3a-21a9df07c45a'
+};
